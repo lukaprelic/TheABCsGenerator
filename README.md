0a2bffa5dec38988845a78db9cf0e7dc9c6432b9
Luka <luka.prelicads@gmail.com>
2021-08-19 16:45:55 +0100

    initial hard coded commit

---
+++ README.md
@@ -0,0 +1,2 @@
+A python script to generate TheABCs NFT project on
+[opensea TheABCs](https://opensea.io/collection/the-abcs)
--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageOps
 
 
-def print_hi(name):
-    img = Image.new('RGB', (100, 30), color=(73, 109, 137))
-
-    fnt = ImageFont.truetype('/Library/Fonts/Arial.ttf', 15)
+def run():
+    img = Image.open(fp='Backgrounds/City.png')
+    img = ImageOps.expand(img, border=10, fill='white')
+    width, height = img.size
+    letter1 = Image.open(fp='Letters/Amber Calligraphy/CA A.png')
+    letter2 = Image.open(fp='Letters/Amber Calligraphy/CA B.png')
+    letter3 = Image.open(fp='Letters/Amber Calligraphy/CA C.png')
+    hat = Image.open(fp='Hats/Gold.png')
+    img.paste(letter1, (0, 350), letter1)
+    img.paste(letter2, (350, 350), letter2)
+    img.paste(letter3, (700, 350), letter3)
+    img.paste(hat, (250, 0), hat)
+    fnt = ImageFont.truetype('arial.ttf', 55)
     d = ImageDraw.Draw(img)
-    d.text((10, 10), "Hello world", font=fnt, fill=(255, 255, 0))
-
-    img.save('pil_text_font.png')
+    d.text((10, 10), "size:("+str(width)+":"+str(height)+")", font=fnt, fill=(255, 255, 0))
+    img.save("test.png")
+    #img.show(img)
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    run()
