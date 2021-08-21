7c6324a5812aa160fb16d18522e147e25ec7f6eb
Luka <luka.prelicads@gmail.com>
2021-08-21 17:50:21 +0100

    new csv file

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ def generateAll():
         letter1 = 'A'
         letter2 = letter2.upper()
         letter3 = 'C'
-        # print(letter2)
         font = 'Amber Sans Serif'
+        hatPath = 'Hats/Gold.png'
         hatOffsets = offsets.Offsets('letterHatOffsets.csv')
-        hatOffsetsForLetter = hatOffsets.getOffsetsForFontLetter(font, letter2)
+        hatName = hatPath[5:-4]
+        hatOffsetsForLetter = hatOffsets.getOffsetsForFontLetter(hatName, font, letter2)
         generateImage(id=index,
                       hatoffsets=hatOffsetsForLetter,
                       font=font,
@@ -23,7 +24,7 @@ def generateAll():
                       letter1Path=('Letters/%s/SaA %s.png' % (font, letter1)),
                       letter2Path=('Letters/%s/SaA %s.png' % (font, letter2)),
                       letter3Path=('Letters/%s/SaA %s.png' % (font, letter3)),
-                      hatPath='Hats/Gold.png',
+                      hatPath=hatPath,
                       addBorder=False)
 
 
@@ -45,29 +46,18 @@ def generateImage(id: int, hatoffsets, font, backgroundPath: string, borderColou
     letterWidth, letterHeight = letter1.size
     letter2 = Image.open(fp=letter2Path)
     letter3 = Image.open(fp=letter3Path)
-    hat = getHatImage(hatPath)
     # needed because the letter sprites are not centered
     letterYOffsetLower = 50
     letterxCoordoffset = 12
-    (hatxOffsetToLetter, hatyOffset, hatScalar, hatRotation) = hatoffsets
-    print('offset:', (hatxOffsetToLetter, hatyOffset, hatScalar, hatRotation))
     letterPasteHeight = int(height / 2) - int(letterHeight / 2) + letterYOffsetLower
-    horizontalLetterWidth = int(width / 3 - letterWidth / 2)
     letter1xCoord = int(width / 3 - letterWidth) + letterxCoordoffset
     letter2xCoord = int(width / 3 * 2 - letterWidth) + letterxCoordoffset
     letter3xCoord = int(width / 3 * 3 - letterWidth) + letterxCoordoffset
-    print((hat.size[0] * hatScalar, hat.size[1] * hatScalar))
-    #change default hat size here
-    hat = hat.resize(map(int, (hat.size[0] * hatScalar * 0.85, hat.size[1] * hatScalar * 0.85)))
-    hat = hat.rotate(hatRotation)
     img.paste(letter1, (letter1xCoord, letterPasteHeight), letter1)
     img.paste(letter2, (letter2xCoord, letterPasteHeight), letter2)
     img.paste(letter3, (letter3xCoord, letterPasteHeight), letter3)
-    #change default hat x coord
-    hatxCoord = letter2xCoord + letterxCoordoffset + hatxOffsetToLetter - 60
-    # change default hat y coord
-    hayyCoord = 160 + hatyOffset
-    img.paste(hat, (hatxCoord, hayyCoord), hat)
+    if any(allowedFonts in font for allowedFonts in ['Serif', 'Sans Serif']):
+        addHat(hatPath, hatoffsets, img, letter2xCoord, letterxCoordoffset)
     finalImageFileName = "Generated/%d ABCs %s%s%s.png" % \
                          (id, letter1Path[-5], letter2Path[-5], letter3Path[-5])
     print("generated image: ", finalImageFileName)
@@ -75,16 +65,29 @@ def generateImage(id: int, hatoffsets, font, backgroundPath: string, borderColou
     # img.show(img)
 
 
+def addHat(hatPath, hatoffsets, img, letter2xCoord, letterxCoordoffset):
+    hat = getHatImage(hatPath)
+    (hatxOffsetToLetter, hatyOffset, hatScalar, hatRotation) = hatoffsets
+    print('csv offset:', (hatxOffsetToLetter, hatyOffset, hatScalar, hatRotation))
+    # change default hat size here
+    hat = hat.resize(map(int, (hat.size[0] * hatScalar * 0.85, hat.size[1] * hatScalar * 0.85)))
+    hat = hat.rotate(hatRotation)
+    # change default hat x coord
+    hatxCoord = letter2xCoord + letterxCoordoffset + hatxOffsetToLetter - 60
+    # change default hat y coord
+    hayyCoord = 160 + hatyOffset
+    # print((hat.size[0] * hatScalar, hat.size[1] * hatScalar))
+    img.paste(hat, (hatxCoord, hayyCoord), hat)
+
+
 # increase size to allow for no rotation cuttofs
 def getHatImage(hatPath):
     hat = Image.open(fp=hatPath)
     (hatWidth, hatHeight) = hat.size
-    print(hatWidth, hatHeight)
     hatLargeCanvas = Image.new(mode='RGBA', size=(int(hatWidth * 1.3), int(hatWidth * 1.3)))
-    #hatLargeCanvas = ImageOps.expand(hatLargeCanvas, border=2, fill='black')
+    # hatLargeCanvas = ImageOps.expand(hatLargeCanvas, border=2, fill='black')
     hatxycoord = (int(hatWidth * (0.3 / 2 + 1) - hatWidth),
                   int(hatHeight * (0.3 / 2 + 1) - hatHeight) - 35)
-    print(hatxycoord)
     hatLargeCanvas.paste(hat, hatxycoord)
     return hatLargeCanvas
 
--- offsets.py
+++ offsets.py
@@ -1,15 +1,18 @@
 import os, pandas
+from enum import Enum
+
+fontColOffset = {'Serif': 0, 'Sans Serif': 1}
 
 
 class Offsets:
     def __init__(self, path):
         self.data = pandas.read_csv(path)
 
-    def getOffsetsForFontLetter(self, font, letter):
+    def getOffsetsForFontLetter(self, hat, font, letter):
         d = self.data
         rowIndex = ord(letter) - ord('A')
-        hatxOffset, hatyOffset, hatScalar, rotation = (d[font][rowIndex].split(","))
-
+        fontWithNoColour = ' '.join(font.split(' ')[1:])
+        hatxOffset, hatyOffset, hatScalar, rotation = (d[hat + ' ' + fontWithNoColour][rowIndex].split(","))
         hatxOffset, hatyOffset, hatScalar, rotation = \
             int(hatxOffset), int(hatyOffset), float(hatScalar), int(rotation)
         return hatxOffset, hatyOffset, hatScalar, rotation
