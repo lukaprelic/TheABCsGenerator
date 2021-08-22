5e4a872a757f5d8d6d13f429e486ba14a92d7deb
Luka <luka.prelicads@gmail.com>
2021-08-22 19:30:12 +0100

    format

--- renameLetters.py
+++ renameLetters.py
@@ -6,5 +6,5 @@ if __name__ == '__main__':
             nameshort = name[-5:]
             join = os.path.join(path, name)
             joinupdated = os.path.join(path, nameshort)
-            print(join,joinupdated)
-            os.rename(join,joinupdated)
+            print(join, joinupdated)
+            os.rename(join, joinupdated)
