a35622a7b9f2af3d8c4c6725c907e42e04c68716
Luka <luka.prelicads@gmail.com>
2021-08-27 22:19:03 +0100

    create nft scripted

--- openseaupload.py
+++ openseaupload.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 import time
 
 
-def uploadFiles():
+def uploadFiles(startItemId):
     chop = webdriver.ChromeOptions()
     chop.add_extension('MetaMask_v10.0.2.crx')
     driver = webdriver.Opera(options=chop)
@@ -14,43 +14,75 @@ def uploadFiles():
     time.sleep(0.5)
     signIntoMeta(driver)
     tabs2 = driver.window_handles
-    print(tabs2)
     driver.switch_to.window(tabs2[1])
-    time.sleep(1)
-
+    time.sleep(2)
+    #print('Starting with item id:', startItemId - 51)
     for index, row in df.iterrows():
-        print(row['ID'], row['BACKGROUND'], row['LETTER1'], row['LETTER2'],
-              row['LETTER3'], row['FONT'], row['HAT'], row['SPECIAL'])
+        print('first row:', index, row)
+        if index > 0:
+            createPage = driver.find_element_by_xpath(
+                '//*[@id="__next"]/div[1]/div[1]/nav/ul/li[4]/a')
+            createPage.click()
+        print("ROW:", row)
+        print(row['Id'], row['Background'], row['Font'], row['Font & Colour Combination'],
+              row['Font Colour'], row['Hat'], row['Letter1'], row['Letter2'],
+              row['Letter3'], row['Letter Permutation'], row['Special'])
         imageUpload = driver.find_element_by_xpath('//*[@id="media"]')
-        imagePath = os.path.abspath('Generated\\51 ABCs ADE None.png')
-        print('image path:', imagePath)
+        imagePath = os.path.abspath('Generated\\51 ABCs SLN None.png')
         imageUpload.send_keys(imagePath)
 
         name = driver.find_element_by_xpath('//*[@id="name"]')
-        name.send_keys(imagePath)
-        name = driver.find_element_by_xpath('//*[@id="name"]')
-        name.send_keys(imagePath)
-        name = driver.find_element_by_xpath('//*[@id="name"]')
-        name.send_keys(imagePath)
-        name = driver.find_element_by_xpath('//*[@id="name"]')
-        name.send_keys(imagePath)
-        name = driver.find_element_by_xpath('//*[@id="name"]')
-        name.send_keys(imagePath)
-        name = driver.find_element_by_xpath('//*[@id="name"]')
-        name.send_keys(imagePath)
+        name.send_keys('The ABCs #{}'.format(row['Id']))
+        description = driver.find_element_by_xpath('//*[@id="description"]')
+        description.send_keys(row['Letter Permutation'])
+        collectionName = driver.find_element_by_xpath(
+            '//*[@id="__next"]/div[1]/main/div/div/section/div/form/section[5]/div/input')
+        collectionName.send_keys('TheABCs')
+        collectionPlusButton = driver.find_element_by_xpath(
+            '//*[@id="__next"]/div[1]/main/div/div/section/div/form/section[6]/div[1]/div/div[2]/button')
+        collectionPlusButton.click()
+        time.sleep(1.5)
+        print('starting properties population')
+        for i, (key, value) in enumerate(row.items()):
+            if key in ['Id', 'Special']:
+                continue
+            collectionAddPropButton = driver.find_element_by_xpath('/html/body/div[2]/div/div/div/section/button')
+            collectionAddPropButton.click()
+            print('index: ', i, i + 1)
+            propInputKeyXpath = (
+                '/html/body/div[2]/div/div/div/section/table/tbody/tr[{}]/td[1]/div/div/input'.format(i + 1))
+            print('propInputKeyXpath', propInputKeyXpath)
+            propKey = driver.find_element_by_xpath(
+                propInputKeyXpath)
+            propKey.send_keys(key)
+            propKey = driver.find_element_by_xpath(
+                ('/html/body/div[2]/div/div/div/section/table/tbody/tr[{}]/td[2]/div/div/input'.format(i + 1)))
+            propKey.send_keys(value)
+        propSave = driver.find_element_by_xpath(
+            '/html/body/div[2]/div/div/div/footer/button')
+        propSave.click()
+        print('completed properties population')
+        time.sleep(0.5)
+        createNFT = driver.find_element_by_xpath(
+            '//*[@id="__next"]/div[1]/main/div/div/section/div/form/div/div[1]/span/button')
+        createNFT.click()
+        print('creating nft ', row['Id'], row['Background'], row['Font'], row['Font & Colour Combination'],
+              row['Hat'], row['Letter Permutation'])
+        time.sleep(5)
+        closeCreateModal = driver.find_element_by_xpath(
+            '/html/body/div[4]/div/div/div/div[2]/button/i')
+        closeCreateModal.click()
 
         time.sleep(500)
 
+
 def signIntoMeta(driver):
     tabs2 = driver.window_handles
-    print(tabs2)
     driver.switch_to.window(tabs2[0])
     time.sleep(0.5)
     # driver.close()
     # driver.switch_to.window(tabs2[0])
-    print(driver.page_source)
     button = driver.find_element_by_xpath('//*[@id="app-content"]/div/div[3]/div/div/div/button')
-    print(button)
     button.click()
     print('meta clicked')
     time.sleep(0.5)
@@ -102,14 +134,14 @@ def signIntoMeta(driver):
     driver.switch_to.window(tabs2[2])
     connectnext = driver.find_element_by_xpath('//*[@id="app-content"]/div/div[3]/div/div[2]/div[4]/div[2]/button[2]')
     connectnext.click()
-    time.sleep(0.1)
+    time.sleep(0.5)
     connect = driver.find_element_by_xpath(
         '//*[@id="app-content"]/div/div[3]/div/div[2]/div[2]/div[2]/footer/button[2]')
     connect.click()
-    time.sleep(1.2)
+    time.sleep(1.5)
     sign = driver.find_element_by_xpath('//*[@id="app-content"]/div/div[3]/div/div[3]/button[2]')
     sign.click()
 
 
 if __name__ == '__main__':
-    uploadFiles()
+    uploadFiles(101)
